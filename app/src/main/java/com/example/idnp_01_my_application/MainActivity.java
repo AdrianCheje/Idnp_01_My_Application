ce78b8770135ac983c0cf8908b03ea9eec7a406d
AdrianCheje <acheje@unsa.edu.pe>
2023-09-23 07:20:54 -0500

    Initial commit

---
+++ app/src/main/java/com/example/idnp_01_my_application/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.idnp_01_my_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
